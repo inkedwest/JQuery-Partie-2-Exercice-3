118eefcc3a2b3139ce6f912d00a412d046e61604
Glatigny <f.west@live.fr>
2019-02-04 12:24:42 +0100

    exo 3

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(function(){ // On appelle la fonction
+  $(document).ready(function(){
+    $('#show').click(function(){
+      $('#text').show();
+    });
+    $('#hide').click(function(){
+      $('#text').hide();
+    });
+  });
+});
